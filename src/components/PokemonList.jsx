76680d934f7779a34796b11323f51c9af682ce2a
Tiffany <139176913+talph101@users.noreply.github.com>
2025-02-18 15:54:29 -0600

    added nav bar

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,29 +1,36 @@
 import React, { useEffect, useState } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import { Container } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
 import axios from 'axios';
 
 
 const PokemonList = () => {
-    const [pokemons, setPokemons] = useState([]);
+  const [pokemons, setPokemons] = useState([]);
 
-    useEffect(() => {
-        const fetchPokemon = async () => {
-            try{
-                const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=151");
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      try {
+        const response = await axios.get(
+          "https://pokeapi.co/api/v2/pokemon?limit=151"
+        );
 
-                setPokemons(response.data.results);
-            } catch (error) {
-                console.error("Error fetching Pokeman data:", error);
+        setPokemons(response.data.results);
+      } catch (error) {
+        console.error("Error fetching Pokeman data:", error);
+      }
+    };
 
-            }
-
-            };
-
-            fetchPokemon();
-        }, []);
+    fetchPokemon();
+  }, []);
 
   return (
     <div className="container">
-      <h1>Pokemon List</h1>
+      <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#">Pokeverse</Navbar.Brand>
+      </Container>
+    </Navbar>
       <ul className="list-group">
         {pokemons.map((pokemon, index) => (
           <li key={index} className="list-group-item">
@@ -32,7 +39,7 @@ const PokemonList = () => {
         ))}
       </ul>
     </div>
-  )
+  );
 }
 
 export default PokemonList
