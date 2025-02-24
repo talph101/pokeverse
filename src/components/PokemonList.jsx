8ee7c9adf8a661e69bc7a585cdf74a08d64c983e
Tiffany <139176913+talph101@users.noreply.github.com>
2025-02-23 20:58:13 -0600

    rendered cards via bootstrap.

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-import { Container } from 'react-bootstrap';
+import { Container, Card, Row, Col } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import axios from 'axios';
 
@@ -27,17 +27,27 @@ const PokemonList = () => {
   return (
     <div className="container">
       <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#">Pokeverse</Navbar.Brand>
-      </Container>
-    </Navbar>
-      <ul className="list-group">
+        <Container>
+          <Navbar.Brand href="#">Pokeverse</Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <Row>
         {pokemons.map((pokemon, index) => (
-          <li key={index} className="list-group-item">
-            {pokemon.name}
-          </li>
+          <Col key={index} sm={4} md={3} lg={2}>
+            <Card className="mb-4">
+              <Card.Img 
+                variant="top" 
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`} 
+                alt={pokemon.name}
+              />
+              <Card.Body>
+                <Card.Title className="text-center">{pokemon.name}</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
         ))}
-      </ul>
+      </Row>
     </div>
   );
 }
